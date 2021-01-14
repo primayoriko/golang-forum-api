f61c60e20913752b1b39bb81d6c51a555f244d16
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-01-14 14:04:38 +0700

    feat: add gorm logging schema

--- api/database/db_connector.go
+++ api/database/db_connector.go
@@ -2,15 +2,37 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
+// ConnectDB used to connect API to the DB
 func ConnectDB() (*gorm.DB, error) {
+	// f, err := os.OpenFile(os.Getenv("DB_LOG_FILEPATH"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	log.Fatalf("error opening file: %v", err)
+	// }
+	// defer f.Close()
+
+	newLogger := logger.New(
+		// log.New(f, "\r\n", log.LstdFlags),
+		log.New(os.Stderr, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second / 100000, // Slow SQL threshold
+			LogLevel:      logger.Silent,        // Log level
+			Colorful:      true,                 // Disable color
+		},
+	)
+
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
 		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 }
